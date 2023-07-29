867aefc2e4e304819f9a4c1af8e3b2093c551505
MateuszKar2 <system505@protonmail.com>
2023-07-29 10:09:31 +0200

    test 01 js

--- src/js/task-11.js
+++ src/js/task-11.js
@@ -1,4 +1,34 @@
-console.log("jestem");
-const baby = ["Adam", "Zosia", "Ula", "Tomek"];
+const numbers = [23, 324, 1, 902, 32];
 
-console.log(baby)
+const message = "It" + "is" + "treu";
+
+console.log(message);
+
+// const guestName = "Mango";
+// const roomNumber = 207;
+// const greeting =
+//   "Welcome " + guestName + ", your room number is " + roomNumber + "!";
+// console.log(greeting);
+
+const guestName = "Mango";
+const roomNumber = 207;
+const greeting = `Welcome ${guestName}, your room number is ${roomNumber}!`;
+console.log(greeting);
+
+console.log(guestName.toLocaleLowerCase());
+console.log(guestName.toLocaleUpperCase());
+
+console.log(guestName.indexOf("r"));
+
+console.log(guestName.includes("r"));
+
+console.log(guestName.endsWith("r"));
+
+console.log(guestName.slice(1, 5));
+
+const age = 26;
+
+console.log(age > 18 && age < 30);
+console.log(age > 18 && age < 20);
+console.log(age > 18 || age < 20);
+console.log(!age)

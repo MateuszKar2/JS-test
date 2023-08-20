6b056f56d0223c9ac85f76a2aa8a1b152ca738ee
MateuszKar2 <system505@protonmail.com>
2023-08-20 13:15:21 +0200

    add file

--- src/js/task-20.js
+++ src/js/task-20.js
@@ -1,5 +1,3 @@
-//czas 3h29min.
-
 //Dwa rodaje programowania
 //PROGRAMOWANIE PROCEDURALNE- 
 //kod jest zestawem niepowiązanych ze sobą funkcji i zmiennych do przechowywania i przetrzymywania informacji
@@ -31,7 +29,7 @@ console.log(employee.getWage());
 //prostszy interfejs, mnijesza złożoność, ułatwiana operatywość
 //OOP => KLASA => to sposób opisania encji (jednostki), opisujący stan i zachowania zależne od tego stanu [Samochód: koła, silnik, podwozie]
 //OOP => OBIEKT =>  określone właściwości i metody danego obiektu [kolor, kształt]
-//oop => INTERFEJS => zestaw właściwości i metod klasy, dostępne podczas pracy z instancją [zwiększanie prędkości, hamowanie]
+//OOP => INTERFEJS => zestaw właściwości i metod klasy, dostępne podczas pracy z instancją [zwiększanie prędkości, hamowanie]
 
 //Prototype 
 //OOP w JS jest zbudowane na dziedziczeniu prototypowym. Obiekty można łączyc w łancuchu dziedziczenia, żeby właściwość nie znaleziona
@@ -39,16 +37,16 @@ console.log(employee.getWage());
 
 //Metoda Object.create(object) tworzy i zwraca nowy obiekt, łącząc go z obiektem
 
-// const animal = {
-//     legs: 4,
-// };
+const animal = {
+    legs: 4,
+};
 
-// const dog = Object.create(animal);
-// dog.name = "Mango";
+const dog = Object.create(animal);
+dog.name = "Mango";
 
 
-// console.log(dog);
-// console.log(animal.isPrototypeOf(dog));
+console.log(dog);
+console.log(animal.isPrototypeOf(dog));
 
 //prototyp jest zapasowym magazynem właściwośći i metod obiektów. Obiekt który działa jako prototyp może mieć swój własny prototyp i tak dalej...
 //w specyfikacji ,,proto" ważne są podwójne nawiasy, oznaczające że jest to właściwość wewnętrzna/usługowa [[ Prototype ]]
@@ -67,17 +65,17 @@ console.log(employee.getWage());
 
 //Metoda hasOwnProperty()- zwróci true jeżeli właściwość należy do tego samego obiektu jeżeli nie false
 
-const animal = { 
-    eats: true 
-};
-const dog = Object.create(animal);
-dog.drinks = true;
+// const animal = { 
+//     eats: true 
+// };
+// const dog = Object.create(animal);
+// dog.drinks = true;
 
-// 1) 
-    for (const key in dog) {
-    if (!dog.hasOwnProperty(key)) continue;
-    console.log(key);
-}
+// // 1) 
+//     for (const key in dog) {
+//     if (!dog.hasOwnProperty(key)) continue;
+//     console.log(key);
+// }
 
 //Metoda Object.keys(obj) zwróci tablicę wyłącznie własnych kluczy obiektu obj, 
 //a więc w praktyce jest używana zamiast for...in bo zwyczajnie jest wygodniejsza i czystsza.
@@ -150,9 +148,59 @@ dog.drinks = true;
 //DZIEDZICZENIE KLAS
 //Słowo kluczowe ,,extends" pozwala na implementacje dziedziczenia klas, gdy jedna klasa dzidziczy właściwości i metody innych klas
 
-class Child extends Parent {
+// class Child extends Parent {
 
-}
+// }
 
 //*klasa potomna nie dziedziczy prywatnych właściwości i metod po klasie nadrzędnej.
   
+
+class Books {
+
+    static Author = {  //właściwości statyczne
+        NAME: "Rowling"
+    }
+    static everyBooks = [];//metody statyczne
+    static isgoodbook(title) {//metody statyczne(zarówno publiczne jak prywatne)
+        return Books.everyBooks.includes(title);
+    }
+
+    author; 
+    #page; //właściwości prywatna
+    constructor({ title, page, author }) {
+        this.title = title;//właściwości publiczne
+        this.#page = page;
+        this.author = author;
+        Books.everyBooks.push(title);
+    }
+
+    getAuthor() {
+        return this.author;
+    }
+
+    setAuthor(newAuthor) {
+        this.author = author;
+    }
+
+    get page() {
+        return this.#page;
+    }
+
+    set page(newPage) {
+        this.#page = newPage;
+    }
+}
+
+const reading = new Books("Harry Potter");
+console.log(reading);//instancja klasy- ponieważ zawiera dane i zachowanie opisane przez klasę
+
+const willread = new Books({//Obiekt parametru
+    title: "Harry Potter",
+    page: "438",
+    author: Books.Author.NAME,
+});
+console.log(willread);
+console.log(willread.page);
+
+
+

bc9d002e49b8cd14cba602ceb4f4df0605c7426f
MateuszKar2 <system505@protonmail.com>
2023-08-23 10:06:03 +0200

    add file

--- src/js/task-22.js
+++ src/js/task-22.js
@@ -1,27 +1,57 @@
 //detektor zdarzeń
-const iAmYes = document.querySelector(".yes");
-iAmYes.addEventListener("click", () => {
-    console.log("Button was clicked");
+const positive = document.querySelector(".yes");
+positive.addEventListener("click", () => {
+    console.log("Lion");
 });
-const iAmNo = document.querySelector(".no");
-iAmNo.addEventListener("click", () => {
-    console.log("Button was not clicked");
+const negative = document.querySelector(".no");
+negative.addEventListener("click", () => {
+    console.log("Horse");
 });
 
 //najlepiej użyc oddzilnej funkcji
 
-const handleClick = () => {
-    console.log("Button was Clickes again")
+const removeClick = () => {
+    console.log("Dog")
 };
 
 //detektor może mieć dowolną liczbę funkcji
 
-const handleClickTwo = () => {
-    console.log("Button was Clickes now")
+const handleClick = () => {
+    console.log("Tiger")
 };
 
 
-iAmYes.addEventListener("click", handleClick);
-iAmYes.addEventListener("click", handleClickTwo);
+positive.addEventListener("click", removeClick);
+positive.addEventListener("click", handleClick);
 
 //usuwanie detektora zdarzeń
+
+positive.removeEventListener("click", removeClick);
+
+//Każde zdarzenie to obiekt
+//Wszystkie zdarzenia pochodzą od klasy bazowej Event.
+// Możemy go nazwać jak chcemy, ale według konwencji jest deklarowany jako e, evt lub event.
+
+const handleClickTwo = (event) => {
+    // console.log(event);
+    console.log(event.type);//click
+    console.log(event.currentTarget);//el. na którym jest wykonywana procedura obsługi zdarzenia
+    event.preventDefault();//aby skasować DOMYŚLNE działanie przeglądarki
+    // console.log(event.preventDefault);
+}
+negative.addEventListener("click", handleClickTwo);
+
+//ZDARZENIA ZWIĄZANE Z KLAWIATURĄ
+
+// Po naciśnięciu klawisza najpierw wydarza się keydown, a po puszczeniu - keyup. 
+//W praktyce, na ogół obsługiwane jest tylko zdarzenie keydown
+
+document.addEventListener("keydown", event => {
+    console.log("Keydown: ", event);
+    console.log("key: ", event.key);//zwraca znak wygenerowany przez naciśnięcie klawisza
+    console.log("code: ", event.code);//zwraca kod fizycznego klawisza na klawiaturze i nie zmienia się w zależności od ustawienia klawiatury użytkownika.
+  });
+  
+document.addEventListener("keyup", event => {
+    console.log("Keyup: ", event);
+  });

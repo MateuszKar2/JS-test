03531c6dff497646bfc7c237f3773f03f71bb999
MateuszKar2 <system505@protonmail.com>
2023-09-19 13:56:50 +0200

    add file

---
+++ src/js/task-25.js
@@ -0,0 +1,11 @@
+const node = "najważniejza funkcjonalność Node.js- możliwość wykonywania kodu po za przeglądarką !!!"
+console.log(node);
+
+const npm = "instaluje on wymagane pakiety i zapewnia przyjazny dla użytkownika interfejs do pracy z nimi"
+console.log(npm);
+
+//NPM składa się z 3 elementów
+//strona npmjs.com- wyszukiwania i dokumentacja pakietów
+//interfejs wiersza poleceń- program uruchamiany w terminalu. Udostepnia zestaw polceń do pracy z rejestrem i pakietami
+//rejestr pakietów - dużapubliczna baza danych narzędzi programistycznych
+

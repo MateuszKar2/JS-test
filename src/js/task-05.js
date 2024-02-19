af4c9a16cd22cfd28df036ea71bf9159a9045ed6
MateuszKar2 <system505@protonmail.com>
2024-02-19 22:35:52 +0100

    add file

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,17 +1,14 @@
 
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
 
-const inputBtn = document.querySelector("#name-input");
-const outputBtn = document.querySelector("#name-output");
-
-
-inputBtn.addEventListener("input", clickInputChange);
-
-function clickInputChange(event) {
-        inputBtn.textContent = event.currentTarget.value;
-    if (event.currentTarget.value.trim() === '') {
-        outputBtn.textContent = 'Anonymous';
+input.addEventListener("input", (e) => {
+    output.textContent = e.currentTarget.value;
+    if(e.currentTarget.value === "") {
+        output.textContent = "Anonymous";
     }
-};
+});
+
 
 
  

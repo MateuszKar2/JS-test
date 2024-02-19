aa7253d53f81a8d3189589cbaba7a0a16dfc87ce
MateuszKar2 <system505@protonmail.com>
2024-02-20 00:08:18 +0100

    add file

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,32 +1,15 @@
-// const textInput = document.querySelector('#validation-input');
-
-
-// textInput.addEventListener('blur', e => {
-//     if (
-//         e.currentTarget.value.lemgth === Number(e.currentTarget.dataset.length)
-//     ) {
-//         textInput.classList.add('valid');
-//         textInput.classList.remove('invalid');
-//     } else {
-//         textInput.classList.add('invalid');
-//         textInput.classList.remove('valid');
-//     }
-// });
-
 
 const textInput = document.querySelector('#validation-input');
 
 
-textInput.addEventListener('blur', event => {
-    if (
-        event.currentTarget.value.length ===
-        Number(event.currentTarget.dataset.length)
-    ) {
-        textInput.classList.add('valid');
-        textInput.classList.remove('invalid');
-    } else {
-        textInput.classList.add('invalid');
-        textInput.classList.remove('valid');
-    }
+textInput.addEventListener('blur', e => {
+if(e.currentTarget.value.length === Number(e.currentTarget.dataset.length)) {
+    textInput.classList.replace("invalid", "valid")
+} else {
+    textInput.classList.add('invalid');
+    textInput.classList.remove('valid');
+}
 });
+
+
  

f81ea680498d0bbcf2dbd2cb9b03189b1679f13d
MateuszKar2 <system505@protonmail.com>
2024-02-20 00:08:29 +0100

    add file

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const mainColor = document.querySelector("body");
+const clickColor = document.querySelector('.change-color');
+const changeColor = document.querySelector('.color');
+
+
+clickColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  mainColor.style.backgroundColor = randomColor;
+  changeColor.textContent = randomColor; 
+})
+

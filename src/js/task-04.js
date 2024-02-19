1536477444c625f0d0a47e849c6ba7f9e8421dab
MateuszKar2 <system505@protonmail.com>
2024-02-19 22:35:49 +0100

    add file

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,21 +1,26 @@
 
 
+
+
 const decrementBtn = document.querySelector('[data-action="decrement');
 const incrementBtn = document.querySelector('[data-action="increment');
 const value = document.querySelector('#value');
 
 let counterValue = 0;
 
+
 decrementBtn.addEventListener("click", () => {
-    counterValue -= 1
-    value.textContent = counterValue
+    value.textContent = counterValue;
+    counterValue -= 1;
+
 });
+
 
 incrementBtn.addEventListener("click", () => {
-    counterValue += 1
-    value.textContent = counterValue
-});
+        value.textContent = counterValue;
+        counterValue += 1;
 
+});
 
 
 

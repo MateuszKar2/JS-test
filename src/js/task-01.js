e5db4366a927a08c5b265ecd594efb9d22eed20d
MateuszKar2 <system505@protonmail.com>
2024-02-19 22:35:41 +0100

    add file

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -11,15 +11,22 @@
 
 
 
-const categories = document.querySelector('#categoies');
+
+
+const category = document.querySelector("#categories");
 const items = document.querySelectorAll('.item');
 
-console.log(`Number of categories ${items.length}`);
+console.log(`categories: ${items.length}`);
+
 
 items.forEach(item => {
-    const title = item.querySelector("h2").textContent;
-    const elements = item.querySelectorAll("li").length;
-
-console.log(`Category: ${title}`)
-console.log(`Elements: ${elements}`)
+        const title = item.querySelector("h2").textContent;
+        const elements = item.querySelectorAll("li").length;
+ 
+        console.log(`Category: ${title}`);
+        console.log(`Elements: ${elements}`);
 });
+
+
+
+
